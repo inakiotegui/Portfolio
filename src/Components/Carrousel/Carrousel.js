aff21d705eb2b7fa0f6ecff38ddb945c3edea049
Iñaki <inakiotegui1@gmail.com>
2021-09-04 17:15:18 -0300

    fixed carrousel bugs

--- src/Components/Carrousel/Carrousel.js
+++ src/Components/Carrousel/Carrousel.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import AliceCarousel from 'react-alice-carousel';
 import 'react-alice-carousel/lib/alice-carousel.css';
+import react from '../../images/technologies/react.png';
+import redux from '../../images/technologies/redux.png';
+import javascript from '../../images/technologies/javascript.png';
+import css from '../../images/technologies/css.png';
+import html from '../../images/technologies/html.png';
+import postgres from '../../images/technologies/postgresql.png';
+import express from '../../images/technologies/express.png';
+import node from '../../images/technologies/nodejs.png';
+import sequelize from '../../images/technologies/sequelize.png';
+
 
 const handleDragStart = (e) => e.preventDefault();
 
 const items = [
-  <img src="https://ensocore.com/media/61/reactjs-logo-sticker%20%281%29.jpg" onDragStart={handleDragStart} role="presentation" alt='react' height="150"/>,
-  <img src="https://lh3.googleusercontent.com/proxy/SF5n8gtw1HguLYpb8vijqR8qlD_pAwQAjO20zQtvjAVgvV1FHW2RQqK9lpA4_E1xGeRYQebsGymClh4TnFp-G4fWvx58uk8fSSELJRGrXeizTssr34N7" onDragStart={handleDragStart} role="presentation" alt='redux' height="150"/>,
-  <img src="https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png" onDragStart={handleDragStart} role="presentation" alt='javascript' width="150"/>,
-  <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png" onDragStart={handleDragStart} role="presentation" alt='css' height="150"/>,
-  <img src="https://www.desarrollolibre.net/public/images/example//html/html5-logo.png" onDragStart={handleDragStart} role="presentation" alt='html' width="150"/>,
-  <img src="https://cpl.thalesgroup.com/sites/default/files/content/paragraphs/intro/2020-03/postgresql-logo.png" onDragStart={handleDragStart} role="presentation" alt='postgresql' width="150"/>,
-  <img src="https://blobscdn.gitbook.com/v0/b/gitbook-28427.appspot.com/o/assets%2F-Lgyno4NC7rhy49BAEjN%2F-Lh14lb3LH4C886qWxYA%2F-Lh1DZeIUQennGd9RiHe%2FScreen%20Shot%202019-06-10%20at%2011.30.20%20AM.png" onDragStart={handleDragStart} role="presentation" alt='express.js' height="120"/>,
-  <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/1280px-Node.js_logo.svg.png" onDragStart={handleDragStart} role="presentation" alt='node.js' height="120"/>,
+  <img src={react} onDragStart={handleDragStart} role="presentation" alt='react' height="150"/>,
+  <img src={redux} onDragStart={handleDragStart} role="presentation" alt='redux' height="150"/>,
+  <img src={javascript} onDragStart={handleDragStart} role="presentation" alt='javascript' width="150"/>,
+  <img src={css} onDragStart={handleDragStart} role="presentation" alt='css' height="150"/>,
+  <img src={html} onDragStart={handleDragStart} role="presentation" alt='html' height="150"/>,
+  <img src={postgres} onDragStart={handleDragStart} role="presentation" alt='postgresql' width="150"/>,
+  <img src={express} onDragStart={handleDragStart} role="presentation" alt='express.js' height="120"/>,
+  <img src={node} onDragStart={handleDragStart} role="presentation" alt='node.js' height="120"/>,
+  <img src={sequelize} onDragStart={handleDragStart} role="presentation" alt='sequelize' height="150"/>,
 ];
 
 const responsive ={
