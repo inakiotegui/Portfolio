7ec5967aad32f842cea0229c4bea9d2730d7d1f0
Iñaki <inakiotegui1@gmail.com>
2021-09-06 21:47:22 -0300

    Update Nav.js

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -64,24 +64,3 @@ export default function NavBar() {
         </div>
     )
 }
-
-/*
-<i class='fas fa-home'></i>
-
-            <div>
-            <NavLink exact to="/">
-                <img id="pokedexLogo" src={pokedex} width="220" height="50" className="d-inline-block align-top" alt="" />
-            </NavLink>
-            </div>
-            <div>
-                <SearchBar/>
-            </div>
-            <nav>
-                <ul className="list">
-                    <li className="list-item">
-                        <NavLink exact to="/home" >Home</NavLink>
-                        <NavLink to="/create" >Create a Pokemon</NavLink>
-                    </li>
-                </ul>
-            </nav>
-*/
